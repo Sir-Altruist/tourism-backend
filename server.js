383775ccb44a3c66251ab8e1b0507d121364b8de
Sir-Altruist <eshooluwasegun98@gmail.com>
2021-08-29 03:16:31 +0100

    heroku issue resolved

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 const dotenv = require('dotenv').config()
 const path = require('path')
 
-const PORT = process.env.NODE_ENV || 5000
+const PORT = process.env.PORT || 5000
 
 
 mongoose.Promise = global.Promise
