74946a0cf22bef6eaebda4b688a8d5e96da8e123
Sir-Altruist <eshooluwasegun98@gmail.com>
2021-10-25 20:16:35 +0100

    final modification

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 const dotenv = require('dotenv').config()
 // const path = require('path')
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 
 mongoose.Promise = global.Promise
@@ -46,4 +46,5 @@ app.get('/paypal', (req, res) => {
 })
 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen()
