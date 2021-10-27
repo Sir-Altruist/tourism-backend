61de46681b25ba70c0d8bb0e4e7487b6f2adef02
Sir-Altruist <eshooluwasegun98@gmail.com>
2021-10-27 01:59:00 +0100

    tired

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const userRoute = require('./routes/user')
 const destinationRoute = require('./routes/destination')
 const uploadRoute = require('./controller/imageUpload')
 const bookingRoute = require('./routes/booking')
-// const cors = require('cors')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 // const path = require('path')
 
@@ -26,23 +26,8 @@ mongoose.connect(
 .then(() => console.log('database connected succesfully'))
 .catch(err => console.log(err))
 
-
-const cors = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://travelfaro.online')
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT')
-    res.header('Access-Control-Max-Age', '3600')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Range')
-    res.header('Access-Control-Expose-Headers', 'Accept-Ranges, Content-Encoding, Content-Length, Content-Range')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    next()
-  }
-
 //allow cross origin resource sharing
-app.use(cors)
-
-app.options('*', (req, res) => {
-    res.send(201)
-})
+app.use(cors())
 
 //form processing
 app.use(express.json());
