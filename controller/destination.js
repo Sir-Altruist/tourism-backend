acf90a8382b10628bf72ee585ad2c04f999a59a2
Sir-Altruist <eshooluwasegun98@gmail.com>
2021-10-27 21:44:10 +0100

    update destination

--- controller/destination.js
+++ controller/destination.js
@@ -88,16 +88,51 @@ exports.editDestination = async (req, res) => {
     try {
 
         const id = req.params.destinationId
-        const { gallery } = req.body
-         //Check for empty field
-         if(!gallery){
-            return res.status(400).json({msg: 'Please fill gallery field'})
-        }
+        const { 
+            name, 
+            location, 
+            duration, 
+            price, 
+            flight, 
+            description, 
+            information, 
+            rating, 
+            gallery, 
+            plan, 
+            facility,
+            cities,
+            p_hotel_name,
+            p_hotel_rating,
+            p_hotel_price,
+            p_hotel_gallery,
+            p_from,
+            p_to,
+            excludes
+            } = req.body
+        
         const destinationDetail = await Destination.findById(id)
         if(!destinationDetail) {
             return res.status(404).json({msg: 'Destination cannot be found'})
         } else {
-            destinationDetail.gallery = gallery
+            destinationDetail.name = name 
+            destinationDetail.location = location 
+            destinationDetail.duration = duration 
+            destinationDetail.price = price 
+            destinationDetail.flight = flight 
+            destinationDetail.description = description 
+            destinationDetail.information = information 
+            destinationDetail.rating = rating 
+            destinationDetail.gallery = gallery 
+            destinationDetail.plan = plan 
+            destinationDetail.facility = facility
+            destinationDetail.cities = cities 
+            destinationDetail.p_hotel_name = p_hotel_name 
+            destinationDetail.p_hotel_rating = p_hotel_rating 
+            destinationDetail.p_hotel_price = p_hotel_price 
+            destinationDetail.p_hotel_gallery = p_hotel_gallery 
+            destinationDetail.p_from = p_from 
+            destinationDetail.p_to = p_to 
+            destinationDetail.excludes = excludes 
 
             //save updated destination information
             const destination = await destinationDetail.save()
